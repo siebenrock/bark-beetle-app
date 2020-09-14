92f21b553e14882225921b4b3779fdb51065edb1
Kai Siebenrock <git@kaisiebenrock.comgit@kaisiebenrock.comgit@kaisiebenrock.comgit@kaisiebenrock.com>
2020-09-14 18:27:14 +0200

    Add measuring, results screen, improve navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,17 @@
 import 'package:borki/screens/screens.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class UserModel {
-  String name = "Jeff";
-}
-
-var stream = Stream.fromIterable([UserModel()]);
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return StreamProvider<UserModel>.value(
-      stream: stream,
-      child: CupertinoApp(
-        title: 'Borki',
-        home: Main(),
-      ),
+    return CupertinoApp(
+      title: 'Woodpinger',
+      home: Main(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
@@ -32,44 +23,40 @@ class Main extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoPageScaffold(
-      child: SafeArea(
-        child: Container(
-          child: CupertinoTabScaffold(
-              tabBuilder: (context, index) {
-                switch (index) {
-                  case 0:
-                    return Measure();
-                    break;
-                  case 1:
-                    return Overview();
-                    break;
-                  case 2:
-                    return Map();
-                    break;
-                  default:
-                }
-              },
-              tabBar: CupertinoTabBar(items: [
-                BottomNavigationBarItem(
-                  icon: Icon(CupertinoIcons.plus_circled),
-                  title: Text("Messung"),
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(CupertinoIcons.book),
-                  title: Text("Übersicht"),
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(CupertinoIcons.location),
-                  title: Text("Karte"),
-                ),
-              ])),
-        ),
-      ),
-      navigationBar: CupertinoNavigationBar(
-        middle: Text("Woodpinger"),
-        trailing: Icon(CupertinoIcons.info),
-      ),
-    );
+    return CupertinoTabScaffold(
+        tabBuilder: (context, index) {
+          switch (index) {
+            case 0:
+              return CupertinoTabView(builder: (context) {
+                return Measure();
+              });
+              break;
+            case 1:
+              return CupertinoTabView(builder: (context) {
+                return Overview();
+              });
+              break;
+            case 2:
+              return CupertinoTabView(builder: (context) {
+                return Map();
+              });
+              break;
+            default:
+          }
+        },
+        tabBar: CupertinoTabBar(items: [
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.plus_circled),
+            title: Text("Messung"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.book),
+            title: Text("Übersicht"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.location),
+            title: Text("Karte"),
+          ),
+        ]));
   }
 }
